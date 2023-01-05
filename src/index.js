1088727d1663941f3900a41be0adae72b850483d
kumar sunny <coolsunny2099@gmail.com>
2023-01-05 12:58:48 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reducer from "./Components/context/reducer";
+import { initialState } from "./Components/context/initialState";
+
+import { StateProvider } from './Components/context/StateProvider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <StateProvider initialState={initialState} reducer={reducer}>
+    <App />
+  </StateProvider>
+
+);
